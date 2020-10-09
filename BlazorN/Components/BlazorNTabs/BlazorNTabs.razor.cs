9a16191b93c8d37c3ceb313a20d7423fc591ea08
Sébastien Bernard <x3900158@apave.com>
2020-10-09 16:11:00 +0200

    add basic tab component to BlazorN

--- BlazorN/Components/BlazorNTabs/BlazorNTabs.razor.cs
+++ BlazorN/Components/BlazorNTabs/BlazorNTabs.razor.cs
@@ -1,11 +1,38 @@
-﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BlazorN;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorN
 {
     public class BlazorNTabsBase : ComponentBase
     {
+        [Parameter] 
+        public RenderFragment ChildContent { get; set; }
+
+        public ITab ActiveTab { get; private set; }
+
+        public void AddTab(ITab tab)
+        {
+            if (ActiveTab == null)
+            {
+                SetActivateTab(tab);
+            }
+        }
+
+        public void RemoveTab(ITab tab)
+        {
+            if (ActiveTab == tab)
+            {
+                SetActivateTab(null);
+            }
+        }
+
+        public void SetActivateTab(ITab tab)
+        {
+            if (ActiveTab != tab)
+            {
+                ActiveTab = tab;
+                StateHasChanged();
+            }
+        }
     }
 }
---
+++ BlazorN/Components/BlazorNTabs/ITab.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorN
+{
+    public interface ITab
+    {
+        RenderFragment ChildContent { get; set; }
+    }
+}
